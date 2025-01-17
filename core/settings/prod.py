ff5ef8229f1cc73187d9830c5f49096f1539f905
mateus-dev-me <mateusmartinsipu2@gmail.com>
2025-01-17 15:54:36 -0300

    fix: changing ALLOWED_HOSTS

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 import json
 
 DEBUG = False
-ALLOWED_HOSTS = ['study_async.mateus-dev-me.com.br']
+ALLOWED_HOSTS = ['studyasync.mateus-dev-me.com.br']
 
 DATABASES = {
     'default': {
